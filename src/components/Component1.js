18a844be2092ec3750025183bea22c769f4269be
Shiyu Wang <shiyu3169@gmail.com>
2019-01-14 11:31:43 -0500

    adding a increasing number in component1

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -17,6 +17,16 @@ export default class Component1 extends Component {
                 dignissimos odio possimus cupiditate maiores quibusdam a,
                 doloremque itaque eos commodi soluta.
               </p>
+              <Spring
+                from={{ number: 0 }}
+                to={{ number: 10 }}
+                config={{ duration: 10000 }}>
+                {props => (
+                  <div style={props}>
+                    <h1 style={counter}>{props.number.toFixed()}</h1>
+                  </div>
+                )}
+              </Spring>
             </div>
           </div>
         )}
@@ -29,4 +39,12 @@ const c1Style = {
   background: "steelblue",
   color: "white",
   padding: "1.5rem"
+};
+
+const counter = {
+  background: "#333",
+  textAlign: "center",
+  width: "100px",
+  borderRadius: "50%",
+  margin: "1rem auto"
 };
