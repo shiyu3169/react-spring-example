2bc23f1f38e80bf165ed9960f6795a84336479cf
Shiyu Wang <shiyu3169@gmail.com>
2019-01-14 11:47:29 -0500

    adding component 3 and transition animation with button click in comonent 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Transition, animated } from "react-spring";
 import Component1 from "./components/Component1";
 import Component2 from "./components/Component2";
+import Component3 from "./components/Component3";
 
 class App extends Component {
+  state = {
+    showCoponent3: false
+  };
+
+  toggle = () => {
+    this.setState({
+      showCoponent3: !this.state.showCoponent3
+    });
+  };
   render() {
     return (
       <div>
         <Component1 />
-        <Component2 />
+        <Component2 toggle={this.toggle} />
+        <Transition
+          native
+          items={this.state.showCoponent3}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}>
+          {show =>
+            show &&
+            (props => (
+              <animated.div style={props}>
+                <Component3 />
+              </animated.div>
+            ))
+          }
+        </Transition>
       </div>
     );
   }
--- src/components/Component2.js
+++ src/components/Component2.js
@@ -18,6 +18,9 @@ export default class Component2 extends Component {
                 dignissimos odio possimus cupiditate maiores quibusdam a,
                 doloremque itaque eos commodi soluta.
               </p>
+              <button style={btn} onClick={this.props.toggle}>
+                Toggle Component 3
+              </button>
             </div>
           </div>
         )}
@@ -30,4 +33,13 @@ const c2Style = {
   background: "slateblue",
   color: "white",
   padding: "1.5rem"
+};
+
+const btn = {
+  background: "#333",
+  color: "#fff",
+  padding: "1rem 2rem",
+  border: "none",
+  textTransform: "uppercase",
+  margin: "15px 0"
 };
